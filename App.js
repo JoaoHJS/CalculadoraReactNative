86fd82a93ce7d6b8a6414d9f17490276834dbca9
JoãoHJS <80012364+JoaoHJS@users.noreply.github.com>
2022-03-25 18:24:34 -0300

    Version 1.0

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity , Text, View } from 'react-native';
+
 
 export default function App() {
+  const operators = ['+', '-', '*', '/', '%', '^', '(', ')']
+  const buttons = [[7, 8, 9, 'AC'], [4,5,6, 'DEL'], [1,2,3, '='], ['', 0, '.', '=']];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.result}>
+
+      </View>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.operatorsContainer}> 
+          {operators.map((operator) => { return <TouchableOpacity key={operator} style={styles.operators}> <Text style={styles.buttonsText}> {operator} </Text></TouchableOpacity>})}
+        </View>
+        <View style={styles.buttonsList}> 
+        
+        </View>
+        <View style={styles.buttonsList}> 
+        
+        </View>
+        <View style={styles.buttonsList}> 
+        
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,4 +37,40 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  result:{
+    flex: 3,
+    width: '100%',
+    backgroundColor: '#05254D'
+  },
+  buttonsContainer:{
+    flex: 5,
+    width: "100%",
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    backgroundColor: '#011936'
+  },
+  operatorsContainer:{
+    flex: 1,
+    height: 100,
+    width: '100%',
+    backgroundColor: "#1153A4"
+  },
+  buttonsList: {
+    flex: 1,
+    height: 100,
+    width: '100%',
+    backgroundColor: "#011936"
+  },
+  operators:{
+    width: '25%',
+    height: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    'justifyContent': 'center',
+    backgroundColor:'red'
+
+  },
+  buttonsText:{
+    color: '#ffffff'
+  }
 });
